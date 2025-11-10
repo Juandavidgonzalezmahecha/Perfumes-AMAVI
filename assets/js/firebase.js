4286e4b03bad5347f1d22bbff1f005cafac70aa1
Juandavidgonzalezmahecha <est.juand.gonzalezm@unimilitar.edu.co>
2025-11-10 09:02:49 -0500

    feat(auth): integración inicial con Firebase + inicio de sesión con email y Google

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,8 +1,7 @@
-// ✅ Inicialización de Firebase para Perfumes AMAVI
+// ✅ Inicialización de Firebase para AMAVI
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
-// Tu configuración de Firebase (ya creada)
 const firebaseConfig = {
   apiKey: "AIzaSyB18GxR5Yc_gkpomNWyrKcx4NNNorcsiic",
   authDomain: "amavi-c8278.firebaseapp.com",
@@ -12,8 +11,9 @@ const firebaseConfig = {
   appId: "1:1001997166005:web:cbc2db2fdd3408a60825fc"
 };
 
-// Inicializar Firebase
+// 🔹 Inicializar Firebase y autenticación
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-// Exportar autenticación
-export const auth = getAuth(app);
+export { app, auth };
+
