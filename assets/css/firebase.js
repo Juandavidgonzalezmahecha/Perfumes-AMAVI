8a24b366098a6dae2c022c4d4fbc02600ac538b0
Juandavidgonzalezmahecha <est.juand.gonzalezm@unimilitar.edu.co>
2025-11-10 08:42:46 -0500

    Update firebase.js

--- assets/css/firebase.js
+++ assets/css/firebase.js
@@ -1,1 +1,19 @@
+// Inicialización de Firebase para AMAVI
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB18GxR5Yc_gkpomNWyrKcx4NNNorcsiic",
+  authDomain: "amavi-c8278.firebaseapp.com",
+  projectId: "amavi-c8278",
+  storageBucket: "amavi-c8278.firebasestorage.app",
+  messagingSenderId: "1001997166005",
+  appId: "1:1001997166005:web:cbc2db2fdd3408a60825fc"
+};
+
+// Inicializa Firebase
+export const app = initializeApp(firebaseConfig);
+
+// Inicializa la autenticación
+export const auth = getAuth(app);
 
